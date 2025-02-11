96a2f1b5ce037327446e4f35465b2b87948e29fd
Quir <quir@arkanus.app>
2025-02-11 17:34:04 -0300

    fix function consumo

--- functions/[[langs]]/itens/[item].js
+++ functions/[[langs]]/itens/[item].js
@@ -1,4 +1,4 @@
-export function onRequest(context) {
+export async function onRequest(context) {
     let valid_langs = ['da','de','en','es','fr','it','nl','no','pl','pt','ru','sv','tr','zh'];
     let atual_lang = Array.isArray(context?.params?.langs) ? context.params.langs[0] : 'en';
 
@@ -7,21 +7,32 @@ export function onRequest(context) {
     }
 
     let item = context.params.item ?? '0';
+    let cacheKey = `${item}-${atual_lang}`;
     let responseBody = `item: ${item} - lang: ${atual_lang}`;
     let etag = `"${btoa(responseBody)}"`;
+
+    // Verifica o cache do Cloudflare
+    const cache = caches.default;
+    let response = await cache.match(cacheKey);
+    if (response) {
+        return response;
+    }
 
     // Se o cliente já tem o conteúdo atualizado, evita reenvio de dados
     if (context.request.headers.get('If-None-Match') === etag) {
         return new Response(null, { status: 304 });
     }
 
-    let response = new Response(responseBody, {
+    response = new Response(responseBody, {
         headers: {
-            'Cache-Control': 'public, max-age=31536000, s-maxage=31536000, immutable',
-            'ETag': etag,
-            'CF-Cache-Status': 'HIT' // Garante que o Cloudflare faça cache
+            'Cache-Control': 'public, max-age=31536000, immutable',
+            'Surrogate-Control': 'max-age=31536000', // Garante que o Cloudflare faça cache
+            'ETag': etag
         }
     });
+
+    // Armazena a resposta no cache do Cloudflare
+    context.waitUntil(cache.put(cacheKey, response.clone()));
 
     return response;
 }

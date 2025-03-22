c814d3a34c17567511e2dd2431e5c36cc324e95f
Quir <quir@arkanus.app>
2025-03-22 02:21:03 -0300

    ordem paranormal post

--- pages/systems/ordem.js
+++ pages/systems/ordem.js
@@ -1,7 +1,7 @@
 const {nav, footer} = require('../../components/navbar')
 const scripts = require('../../components/bootscripts')
 const {head} = require('../../components/head')
-const {blog, PlaceParagraphs, TextAndImage} = require('../../components/blogpost')
+const {blog, PlaceParagraphs, TextAndImage, MarkdownContent} = require('../../components/blogpost')
 
 function linkReplacer(string) {
     // transforme [link](url) em <a href="url">link</a>
@@ -21,8 +21,7 @@ ${head(`${t.lang}${rota}`,`${t.system.default.title.replaceAll("%system%",sistem
     ${nav(t, rota)}
 
     ${blog(t.system.default.title.replaceAll("%system%",sistema.card.title),sistema.tags,sistema.bg,`
-    ${PlaceParagraphs(t.tos[0].title,sistema.article)}
-    ${PlaceParagraphs(t.system.default.playDiscord.replaceAll("%system%",sistema.card.title),t.system.default.playDiscordDesc.replaceAll("%system%",sistema.card.title))}
+    ${MarkdownContent(sistema.md)}
     ${TextAndImage(t.footer.legal,linkReplacer(sistema.legal),sistema.icon,t.system.default.title.replaceAll("%system%",sistema.card.title))}
 
 

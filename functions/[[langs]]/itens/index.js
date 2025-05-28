5040894dfe6eb07e5aa22095350bebcbfaee88af
Quir <quir@arkanus.app>
2025-05-28 00:03:15 -0300

    fix cachê

--- functions/[[langs]]/itens/index.js
+++ functions/[[langs]]/itens/index.js
@@ -1,8 +1,12 @@
 export function onRequest(context) {
-    let valid_langs = ['da','de','en','es','fr','it','nl','no','pl','pt','ru','sv','tr','zh']
-    let atual_lang = Array.isArray(context?.params?.langs) ? context.params.langs[0] : 'en'
-    if (!valid_langs.includes(atual_lang)) {
-      return new Response(null, { status: 302, headers: { 'Location': '/404' } })
-    }
-    return new Response("Hello, world!")
+  let valid_langs = ['da','de','en','es','fr','it','nl','no','pl','pt','ru','sv','tr','zh'];
+  let atual_lang = Array.isArray(context?.params?.langs) ? context.params.langs[0] : 'en';
+  if (!valid_langs.includes(atual_lang)) {
+    return new Response(null, { status: 302, headers: { 'Location': '/404' } });
   }
+  return new Response("Hello, world!", {
+    headers: {
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=60'
+    }
+  });
+}

8f897aeb6a9f3f3ded0ac539a96decd51ffb9607
Quir <quir@arkanus.app>
2025-11-16 22:54:25 -0300

    fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,6 @@ export default defineConfig({
       minify: 'terser',
       terserOptions: {
         compress: {
-          drop_console: true,
           drop_debugger: true,
           pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
         },
